da795969626b166310991e62c97824c1e73cb8fe
sanszhu <sanszhu@tencent.com>
2019-08-23 17:12:16 +0800

    矩阵运算
    Co-Authored-By: 朱哲 <466648706@qq.com>

---
+++ graph/GraphtoMatrix.js
@@ -0,0 +1,59 @@
+/*
+ 用临接链表表示图
+
+var { Vertex, Edge } = require('./graph-example.js');
+ */
+var Vertex = [
+           { n: "a" },{ n: "b" },{ n: "c" },{ n: "d" },
+           { n: "e" },{ n: "f" },{ n: "g" },{ n: "h" },
+           { n: "i" },{ n: "j" },{ n: "k" },{ n: "l" }
+       ];
+
+var Edge = [
+  { s: "a" ,e: "b", w: 1 },
+  { s: "a" ,e: "c", w: 1 },
+  { s: "a" ,e: "d", w: 2 },
+  { s: "b" ,e: "c", w: 2 },
+  { s: "b" ,e: "d", w: 2 },
+  { s: "b" ,e: "e", w: 2 },
+  { s: "i" ,e: "j", w: 3 },
+  { s: "g" ,e: "h", w: 3 },
+  { s: "b" ,e: "h", w: 3 },
+  { s: "i" ,e: "l", w: 3 },
+  { s: "d" ,e: "j", w: 1 },
+  { s: "e" ,e: "f", w: 1 },
+  { s: "c" ,e: "k", w: 1 },
+  { s: "l" ,e: "k", w: 1 },
+  { s: "g" ,e: "e", w: 1 },
+  { s: "f" ,e: "d", w: 1 }
+]
+
+
+var _buildMatrix = (Vertex, Edge) => {
+  var _subMark = {};
+  var _len = Vertex.length;
+  //  初始化矩阵
+  var _buildRow = (item, length) => {
+    var _res = [];
+    for ( var i = 0; i < _len; i++ ) {
+      // B 如果是对象 需要深拷贝 不然是指针 循环了n次
+      if ( typeof item === "object") {
+        item = item.concat([]);
+      }
+      _res.push(item)
+    }
+    return _res
+  }
+  var _res = _buildRow(_buildRow(0, _len), _len);
+
+  Vertex.forEach((e, i) => {
+    _subMark[e.n] =  i
+  })
+
+  Edge.forEach(e => {
+    _res[_subMark[e.s]][_subMark[e.e]] = e.w
+  })
+
+  return _res;
+}
+_buildMatrix( Vertex, Edge );
